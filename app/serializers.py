18c2e249f1ee9eae9560a0eae19016f5bd8cf127
andriihrechko <andriihrechko250703@gmail.com>
2026-06-23 11:46:23 +0300

    fix type annotations for serialize

--- app/serializers.py
+++ app/serializers.py
@@ -7,19 +7,19 @@ from app.book import Book
 
 class BookSerializer(ABC):
     @abstractmethod
-    def serialize(self, book: Book) -> None:
+    def serialize(self, book: Book) -> str:
         pass
 
 
 class JSONSerializer(BookSerializer):
-    def serialize(self, book: Book) -> None:
+    def serialize(self, book: Book) -> str:
         return json.dumps(
             {"title": book.title, "content": book.content}
         )
 
 
 class XMLSerializer(BookSerializer):
-    def serialize(self, book: Book) -> None:
+    def serialize(self, book: Book) -> str:
         root = ElementTree.Element("book")
         ElementTree.SubElement(root, "title").text = book.title
         ElementTree.SubElement(root, "content").text = book.content
